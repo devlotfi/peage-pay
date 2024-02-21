88383acf552ef15dbc3e26ab63b4515b7f198257
devlotfi <158048878+devlotfi@users.noreply.github.com>
2024-02-21 11:36:28 +0100

    update

--- peage-pay-server/src/app.module.ts
+++ peage-pay-server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { RedisModule } from './redis/redis.module';
+import { AuthModule } from './auth/auth.module';
+import { EmailModule } from './email/email.module';
+import { SmsModule } from './sms/sms.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, RedisModule, AuthModule, EmailModule, SmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
