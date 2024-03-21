d6e51360873ad74ff02be3b5a0de2714603e4fe3
devlotfi <158048878+devlotfi@users.noreply.github.com>
2024-03-21 08:49:05 +0100

    started migrating to yarn workspaces

--- peage-pay-web/client/auth-common-client/src/main.tsx
+++ peage-pay-web/client/auth-common-client/src/main.tsx
@@ -1,20 +1,10 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
-import { ThemeProvider } from '@peage-pay-web/tailwind-config';
-import './i18n';
-import { AuthProvider } from '@peage-pay-web/auth';
-import { ApplicationApolloClientProvider } from '@peage-pay-web/apollo-client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <StrictMode>
-    <ThemeProvider>
-      <ApplicationApolloClientProvider>
-        <App />
-      </ApplicationApolloClientProvider>
-    </ThemeProvider>
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- peage-pay-web/client/general-admin-panel/src/main.tsx
+++ peage-pay-web/client/general-admin-panel/src/main.tsx
@@ -1,23 +1,10 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
-import { ThemeProvider } from '@peage-pay-web/tailwind-config';
-import './i18n';
-import { AuthProvider } from '@peage-pay-web/auth';
-import { ApplicationApolloClientProvider } from '@peage-pay-web/apollo-client';
-import { BaseUserRolesType } from './__generated__/graphql';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <StrictMode>
-    <ThemeProvider>
-      <ApplicationApolloClientProvider>
-        <AuthProvider allowedRoles={[BaseUserRolesType.GeneralAdmin]}>
-          <App />
-        </AuthProvider>
-      </ApplicationApolloClientProvider>
-    </ThemeProvider>
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
