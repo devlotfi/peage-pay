142d918c7f18e09e24d58cdc56142eaf4f19ee22
devlotfi <158048878+devlotfi@users.noreply.github.com>
2024-04-12 19:04:41 +0100

    test

--- peage-pay-server/src/payment/payment.controller.ts
+++ peage-pay-server/src/payment/payment.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller()
 export class PaymentController {
   @Post('webhook')
-  public handleWebHook(@Body() body: any) {
+  public handleWebHook(@Body() body: any, @Req() req: Request) {
     console.log(JSON.stringify(body));
+    console.log(req.get('signature'));
 
     return 'Hello world';
   }
