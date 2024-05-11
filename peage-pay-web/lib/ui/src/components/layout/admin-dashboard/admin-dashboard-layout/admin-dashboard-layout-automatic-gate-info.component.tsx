19b281e07f19d1e1698809b0652b7d1ea34e3b64
devlotfi <158048878+devlotfi@users.noreply.github.com>
2024-05-11 09:41:28 +0100

    fixed automatic gate

--- peage-pay-web/lib/ui/src/components/layout/admin-dashboard/admin-dashboard-layout/admin-dashboard-layout-automatic-gate-info.component.tsx
+++ peage-pay-web/lib/ui/src/components/layout/admin-dashboard/admin-dashboard-layout/admin-dashboard-layout-automatic-gate-info.component.tsx
@@ -4,9 +4,7 @@ import { Utils } from '@peage-pay-web/utils';
 import { AutomaticGateType } from '../../../../__generated__/graphql';
 import Table from '../../../elements/table/table.component';
 
-const adminDashboardLayoutAutomaticGateInfoVariants = cva(
-  'flex flex-col bg-base-100 rounded-lg border-[1px] border-edge-200 mb-[1rem]',
-);
+const adminDashboardLayoutAutomaticGateInfoVariants = cva('flex flex-col');
 
 interface AdminDashboardLayoutAutomaticGateInfoProps
   extends BaseHTMLAttributes<HTMLDivElement>,
@@ -27,26 +25,34 @@ const AdminDashboardLayoutAutomaticGateInfo = ({
       )}
       {...props}
     >
-      <Table>
-        <Table.Body>
-          <Table.Head.Tr>
-            <Table.Head.Th className="py-[0.2rem]">Direction</Table.Head.Th>
-          </Table.Head.Tr>
-          <Table.Body.Tr>
-            <Table.Body.Td className="py-[0.2rem]">
-              {automaticGate.direction}
-            </Table.Body.Td>
-          </Table.Body.Tr>
-          <Table.Head.Tr>
-            <Table.Head.Th className="py-[0.2rem]">Toll</Table.Head.Th>
-          </Table.Head.Tr>
-          <Table.Body.Tr>
-            <Table.Body.Td className="py-[0.2rem]">
-              {automaticGate.toll.name}
-            </Table.Body.Td>
-          </Table.Body.Tr>
-        </Table.Body>
-      </Table>
+      <div className="flex flex-col bg-base-100 rounded-lg border-[1px] border-edge-200 mb-[0.5rem]">
+        <Table>
+          <Table.Body>
+            <Table.Head.Tr>
+              <Table.Head.Th className="py-[0.2rem]">Toll</Table.Head.Th>
+            </Table.Head.Tr>
+            <Table.Body.Tr>
+              <Table.Body.Td className="py-[0.2rem]">
+                {automaticGate.toll.name}
+              </Table.Body.Td>
+            </Table.Body.Tr>
+          </Table.Body>
+        </Table>
+      </div>
+      <div className="flex flex-col bg-base-100 rounded-lg border-[1px] border-edge-200 mb-[1rem]">
+        <Table>
+          <Table.Body>
+            <Table.Head.Tr>
+              <Table.Head.Th className="py-[0.2rem]">Direction</Table.Head.Th>
+            </Table.Head.Tr>
+            <Table.Body.Tr>
+              <Table.Body.Td className="py-[0.2rem]">
+                {automaticGate.direction}
+              </Table.Body.Td>
+            </Table.Body.Tr>
+          </Table.Body>
+        </Table>
+      </div>
     </div>
   );
 };
